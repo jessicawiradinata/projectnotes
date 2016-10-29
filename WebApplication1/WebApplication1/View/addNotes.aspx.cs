4c936442d20bad4a7a88c49a192594ca370acaea
timothyalfares <timothy4173@gmail.com>
2016-10-29 18:50:12 +1100

    addnotes added

--- WebApplication1/WebApplication1/View/addNotes.aspx.cs
+++ WebApplication1/WebApplication1/View/addNotes.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace WebApplication1.View
 {
-    public partial class main : System.Web.UI.Page
+    public partial class addNotes : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
