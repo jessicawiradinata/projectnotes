81535f70a20b4569d843c5aa22e9a3ac4b4d2136
Jessica <jessicawiradinata@gmail.com>
2016-10-31 11:34:46 +1100

    index.aspx.cs - verify login refactored

--- WebApplication1/WebApplication1/View/index.aspx.cs
+++ WebApplication1/WebApplication1/View/index.aspx.cs
@@ -17,6 +17,33 @@ namespace WebApplication1.View
 
         protected void login_click(object sender, EventArgs e)
         {
+            verifyLogin();           
+        }
+
+        public static string MD5Hash(string input)
+        {
+            StringBuilder hash = new StringBuilder();
+            MD5CryptoServiceProvider md5provider = new MD5CryptoServiceProvider();
+            byte[] bytes = md5provider.ComputeHash(new UTF8Encoding().GetBytes(input));
+
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                hash.Append(bytes[i].ToString("x2"));
+            }
+            return hash.ToString();
+        }
+
+        public Boolean checkPassword(string pass1, string pass2)
+        {
+            if (pass1.Equals(pass2))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void verifyLogin()
+        {
             String usernameField = username.Text;
             String passwordField = MD5Hash(password.Text);
 
@@ -41,28 +68,6 @@ namespace WebApplication1.View
                 Response.Redirect("index.aspx");
             }
             conn.Close();
-        }
-
-        public static string MD5Hash(string input)
-        {
-            StringBuilder hash = new StringBuilder();
-            MD5CryptoServiceProvider md5provider = new MD5CryptoServiceProvider();
-            byte[] bytes = md5provider.ComputeHash(new UTF8Encoding().GetBytes(input));
-
-            for (int i = 0; i < bytes.Length; i++)
-            {
-                hash.Append(bytes[i].ToString("x2"));
-            }
-            return hash.ToString();
-        }
-
-        public Boolean checkPassword(string pass1, string pass2)
-        {
-            if (pass1.Equals(pass2))
-            {
-                return true;
-            }
-            return false;
         }
     }
 }
